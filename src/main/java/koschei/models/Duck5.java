5b7420c16e749810782282709f10a1e0b48ee329
Kristina I <engels-kristina@mail.ru>
2026-06-24 14:02:03 +0500

    Исправленная задача 2_1_4

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -6,15 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Duck5 {
 
-    private Egg6 egg;
-
     @Autowired
-    public Duck5(Egg6 egg) {
-        this.egg = egg;
-    }
+    private Egg6 egg6;
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + egg.toString();
+        return ", в утке яйцо " + egg6.toString();
     }
 }
